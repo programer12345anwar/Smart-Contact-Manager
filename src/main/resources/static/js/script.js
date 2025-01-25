babc8e386ef531a3b4a5cb6483f96d5440a47ccf
programer12345anwar <mdanwar40212@gmail.com>
2025-01-26 02:45:10 +0530

    updated ui for mobile

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,2 +1,11 @@
 console.log("this is script file");
 //alert("this is for testing");
+
+document.querySelector('.navbar-toggler').addEventListener('click', function () {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse.classList.contains('show')) {
+        navbarCollapse.style.maxHeight = '0'; // Collapse effect
+    } else {
+        navbarCollapse.style.maxHeight = '300px'; // Expand effect
+    }
+});
